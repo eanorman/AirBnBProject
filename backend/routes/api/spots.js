276fd7d00eebbe12efc2357319b78c29e4fb3a29
Emily <emily.norman@outlook.com>
2023-06-06 21:21:54 -0400

    add api endpoint to create a spot

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -9,10 +9,81 @@ const { User, Spot } = require('../../db/models');
 const router = express.Router();
 
 
-// Sign up
+const validateSpot = [
+    check('address')
+      .exists({ checkFalsy: true })
+      .withMessage('Street address is required'),
+    check('address')
+      .isAlphanumeric('en-US', {  ignore: ' '  })
+      .withMessage('Must be a valid address'),
+    check('city')
+      .exists({ checkFalsy: true })
+      .withMessage('City is required'),
+    check('city')
+      .isAlpha('en-US', {  ignore: ' '  })
+      .withMessage('Must be a valid city'),
+    check('state')
+      .exists({ checkFalsy: true })
+      .withMessage('State is required'),
+    check('state')
+      .isAlpha('en-US', {  ignore: ' '  })
+      .withMessage('Must be a valid state'),
+    check('country')
+      .exists({ checkFalsy: true })
+      .withMessage('Country is required'),
+    check('country')
+      .isAlpha('en-US', {  ignore: ' '  })
+      .withMessage('Must be a valid country'),
+    check('lat')
+      .exists({ checkFalsy: true })
+      .isDecimal({ decimal_digits: '1,7' })
+      .withMessage('Latitude is not valid'),
+    check('lng')
+      .exists({ checkFalsy: true })
+      .isDecimal({ decimal_digits: '1,7' })
+      .withMessage('Longitude is not valid'),
+    check('name')
+      .exists({ checkFalsy: true })
+      .withMessage('Name is required'),
+    check('name')
+      .isLength({  max: 50  })
+      .withMessage('Name must be less than 50 characters'),
+    check('description')
+      .exists({ checkFalsy: true })
+      .withMessage('Description is required'),
+    check('price')
+      .exists({ checkFalsy: true })
+      .withMessage('Price per day is required'),
+    check('price')
+      .isNumeric()
+      .withMessage('Price must be a valid number'),
+    handleValidationErrors
+  ];
+
+// Get all spots
 router.get('/', async (req, res) => {
     let spots = await Spot.findAll();
     res.json(spots)
 });
+
+// Create a spot
+router.post('/', validateSpot, requireAuth, async (req, res, next) => {
+    const { address, city, state, country, lat, lng, name, description, price } = req.body
+    const { user } = req;
+     let spot = await Spot.create({
+        ownerId: user.id,
+        address,
+        city,
+        state,
+        country,
+        lat,
+        lng,
+        name,
+        description,
+        price
+     })
+
+     res.json(spot);
+})
 
 module.exports = router;
