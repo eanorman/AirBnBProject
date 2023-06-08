d882979b6fcce7063c74c7e76735cf7f5d89db60
Emily <emily.norman@outlook.com>
2023-06-07 21:05:49 -0400

    add endpoint to get review by current user

---
+++ backend/routes/api/reviews.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+const  sequelize  = require('sequelize')
+const { getReviewSpot, getReviewUser, getReviewImages } = require('../../utils/review')
+
+const { setTokenCookie, requireAuth, spotOwner } = require('../../utils/auth');
+const { User, Spot, Review, SpotImage } = require('../../db/models');
+const { getSpotImages } = require('../../utils/spot');
+
+const router = express.Router();
+
+router.get('/current', requireAuth, async (req, res, next) => {
+    let { user } = req;
+
+    let reviews = await Review.findAll({
+        where: {
+            userId: user.id
+        }
+    })
+
+    await getReviewUser(reviews)
+    await getReviewSpot(reviews);
+    await getReviewImages(reviews);
+
+    res.json({
+        Reviews: reviews
+    })
+})
+
+module.exports = router;
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const bcrypt = require('bcryptjs');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
-const  sequelize  = require('sequelize')
 const { spotsWithPreview,spotsWithAverage, numberOfReviews, addAvgStarRating, getSpotImages, getSpotOwner } = require('../../utils/spot')
-
-const { setTokenCookie, requireAuth, spotOwner } = require('../../utils/auth');
-const { User, Spot, Review, SpotImage } = require('../../db/models');
+const { requireAuth, spotOwner } = require('../../utils/auth');
+const { Spot, SpotImage } = require('../../db/models');
 
 const router = express.Router();
 
