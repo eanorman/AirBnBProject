36ed57be4f3813713c1160d775a1fca08eada50e
Emily <emily.norman@outlook.com>
2023-06-10 18:22:17 -0400

    update migration files

--- backend/db/migrations/20230606223455-create-spot.js
+++ backend/db/migrations/20230606223455-create-spot.js
@@ -64,9 +64,10 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Spots');
+    options.tableName = "Spots";
+    return queryInterface.dropTable(options);
   }
 };
--- backend/db/migrations/20230606225006-create-booking.js
+++ backend/db/migrations/20230606225006-create-booking.js
@@ -39,9 +39,10 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Bookings');
+    options.tableName = "Bookings";
+    return queryInterface.dropTable(options);
   }
 };
